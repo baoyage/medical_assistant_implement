3ea6ab65738e3aadcbbb55658543ed0490afcd80
baoyage <baoyage@gmail.com>
2020-12-05 14:28:09 +0800

    map and GPS

---
+++ app/src/main/java/syr/project/medical_assistant_implement/MyClusterItem.java
@@ -0,0 +1,35 @@
+package syr.project.medical_assistant_implement;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+public class MyClusterItem implements ClusterItem {
+    private final LatLng mPosition;
+    private String mTitle;
+    private String mSnippet;
+
+    public MyClusterItem(double lat, double lng) {
+        mPosition = new LatLng(lat, lng);
+    }
+
+    public MyClusterItem(double lat, double lng, String title, String snippet) {
+        mPosition = new LatLng(lat, lng);
+        mTitle = title;
+        mSnippet = snippet;
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+
+    @Override
+    public String getTitle() {
+        return mTitle;
+    }
+
+    @Override
+    public String getSnippet() {
+        return mSnippet;
+    }
+}
